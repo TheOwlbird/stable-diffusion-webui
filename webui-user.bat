e1a58b6a51ca00935f455e328d7d1867405f82e5
Emanuel B <50232770+TheOwlbird@users.noreply.github.com>
2023-12-12 02:03:47 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,9 +1,8 @@
-git pull
 @echo off
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --listen --port 1337 --no-half-vae
-
+::set COMMANDLINE_ARGS= --listen --port 556 --no-half-vae
+set COMMANDLINE_ARGS= --no-half-vae
 call webui.bat
